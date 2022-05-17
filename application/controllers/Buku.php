bdef7ce8cb325060af248d61aed7be9c5ca5e6c4
AripBudiman12 <arip24.ab@gmail.com>
2022-05-17 22:02:58 +0700

    membetulkan join kategori models & controller buku

--- application/controllers/Buku.php
+++ application/controllers/Buku.php
@@ -132,7 +132,7 @@ class Buku extends CI_Controller
         $kategori = $this->ModelBuku->joinKategoriBuku(['buku.id' => $this->uri->segment(3)])->result_array();
         foreach ($kategori as $k) {
         $data['id'] = $k['id_kategori'];
-        $data['k'] = $k['kategori'];
+        $data['k'] = $k['id_kategori'];
     }
     $data['kategori'] = $this->ModelBuku->getKategori()->result_array();
     $this->form_validation->set_rules('judul_buku', 'Judul 
